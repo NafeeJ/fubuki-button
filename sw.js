a4c2ced2ab7dc72c1080869e3c136aecfd6040a8
lonelyion <lonelyion@outlook.com>
2020-07-13 03:31:29 +0000

    Deploying to gh-pages from  @ 386af860a6026fbdabaa00377d02ed1f8aca0ffd 🚀

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('https://btn.lonelyion.com/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('https://fubuki.lonelyion.com/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
